c80907404c6eae9b288eeceb309866715978c362
Ezra <ezraholm50@gmail.com>
2016-01-16 19:10:21 +0100

    Update pre-img-cmd.sh

--- pre-img-cmd.sh
+++ pre-img-cmd.sh
@@ -10,4 +10,4 @@ sudo apt-get install -y net-tools sudo nano git linux-firmware dnsutils language
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 sudo useradd -d /home/$USERNAME -m $USERNAME && sudo usermod -aG sudo $USERNAME && echo $USERNAME:$USERPASS | chpasswd
 sed -i 's|    SendEnv LANG LC_*|#   SendEnv LANG LC_*|g' /etc/ssh/ssh_config
-sed -i 's|AcceptEnv LANG LC_*|#AcceptEnv LANG LC_*|g' /etc/ssh/sshd_confi
+sed -i 's|AcceptEnv LANG LC_*|#AcceptEnv LANG LC_*|g' /etc/ssh/sshd_config
