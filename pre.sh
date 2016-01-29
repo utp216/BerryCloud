67a42e13f5692331df836a7183c20673fd4c811a
Ezra <ezraholm50@gmail.com>
2016-01-29 18:05:42 +0100

    add raspi config

--- pre.sh
+++ pre.sh
@@ -7,4 +7,12 @@ USERPASS=owncloud
 
 sudo apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get autoclean -y
 sudo useradd -d /home/$USERNAME -m $USERNAME && sudo usermod -aG sudo $USERNAME && echo $USERNAME:$USERPASS | chpasswd
+userdel ubuntu
+rm -rf /home/ubuntu
+cd /tmp
+wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150131-5_all.deb
+apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1
+dpkg -i raspi-config_20150131-5_all.deb
+raspi-config
+
 sudo reboot
