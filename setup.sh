8e8be186b107dca31361384e4e6137331082e660
Ezra <ezraholm50@gmail.com>
2016-02-01 00:26:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -191,7 +191,8 @@ fi
 sudo apt-get update
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 sudo apt-get update
-sudo apt-get install -y software-properties-common ifupdown python-software-properties net-tools sudo nano git linux-firmware dnsutils language-pack-en-base expect aptitude dialog lvm2 ntp curl initscripts keyboard-configuration
+sudo apt-get install -y software-properties-common ifupdown python-software-properties net-tools sudo dnsutilsnano git linux-firmware dnsutils language-pack-en-base expect aptitude dialog lvm2 ntp curl initscripts keyboard-configuration
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoclean && apt-get clean -y && apt-get autoremove -y && apt-get -f install -y
 
 # Remove locale error over ssh in other language
 sed -i 's|    SendEnv LANG LC_*|#   SendEnv LANG LC_*|g' /etc/ssh/ssh_config
