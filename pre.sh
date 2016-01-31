7185f6e84d6476965263b42fa31759d26272ab1b
Ezra <ezraholm50@gmail.com>
2016-01-31 19:41:19 +0100

    automated resize of sdcard

--- pre.sh
+++ pre.sh
@@ -9,10 +9,20 @@ sudo apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get au
 sudo useradd -d /home/$USERNAME -m $USERNAME && sudo usermod -aG sudo $USERNAME && echo $USERNAME:$USERPASS | chpasswd
 userdel ubuntu
 rm -rf /home/ubuntu
+fdisk $device << EOF
+d
+2
+n
+p
+2
+
+
+w
+EOF
 cd /tmp
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150131-5_all.deb
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 -y
 dpkg -i raspi-config_20150131-5_all.deb
 raspi-config
-fdisk /dev/mmcblk0
+
 sudo reboot
