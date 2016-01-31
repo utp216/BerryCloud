569b29f34fd8b6fa002804af6803fa7fd53cc660
Ezra <ezraholm50@gmail.com>
2016-02-01 00:06:46 +0100

    Update connection.sh

--- connection.sh
+++ connection.sh
@@ -1,5 +1,6 @@
 sudo ifdown -a
 sudo ifup -a
 mkdir /var/scripts
+sudo wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/setup.sh -P /var/scripts
 sudo wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/pre.sh -P /var/scripts
 sudo bash /var/scripts/pre.sh
