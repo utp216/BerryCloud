c12690c964b50f4f7b4866409c8cc089d9ee0196
Ezra <ezraholm50@gmail.com>
2016-01-31 23:48:20 +0100

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -7,8 +7,8 @@ USERPASS=owncloud
 
 sudo apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get autoclean -y
 #sudo useradd -d /home/$USERNAME -m $USERNAME && sudo usermod -aG sudo $USERNAME && echo $USERNAME:$USERPASS | chpasswd
-userdel ubuntu
-rm -rf /home/ubuntu
+#userdel ubuntu
+#rm -rf /home/ubuntu
 fdisk $device << EOF
 d
 2
@@ -24,5 +24,4 @@ wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 -y
 dpkg -i raspi-config_20150131-5_all.deb
 raspi-config
-
 sudo reboot
