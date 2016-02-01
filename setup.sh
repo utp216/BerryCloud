b1af86af30dd80f61b66d7a9e35c46b6278617a3
Ezra <ezraholm50@gmail.com>
2016-02-01 09:57:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -657,11 +657,25 @@ function ask_yes_or_no() {
         *)     echo "no" ;;
     esac
 }
-if [[ "yes" == $(ask_yes_or_no "We are now setting up your USB harddrive to mount the os, please attach your USB harddrive now, if you have not done so and type yes.") ]]
+if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition? Also attach it before typing yes!!") ]]
 then
+# Format and create partition
+fdisk /dev/sda1 << EOF
+wipefs
+EOF
+fdisk /dev/sda1 << EOF
+0
+n
+p
+1
+
+
+w
+EOF
+	sudo mkfs.ext4 /dev/sda1 -y
 	dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda1
-	sed -i 's|root=/dev/mmcblk0p2|root=/dev/sda1|g' /boot/cmdline.txt
-#else
+	sed -i 's|/dev/mmcblk0p2|/dev/sda1|g' /etc/fstab
+else
 echo
     echo "If you want to do this later: bash /var/scripts/usbhd.sh"
     echo -e "\e[32m"
