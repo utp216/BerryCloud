f9ff726348bf1380cf0b77078d0c52bbc5712d0b
Ezra <ezraholm50@gmail.com>
2016-02-01 10:04:18 +0100

    Update usbhd.sh

--- usbhd.sh
+++ usbhd.sh
@@ -9,6 +9,10 @@ function ask_yes_or_no() {
 if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition? Also attach it before typing yes!!") ]]
 then
 # Format and create partition
+	echo -e "\e[32m"
+	read -p "If it asks to overwrite anything just hit yes, make sure there are no needed files on the hd. Press any key to start the script..." -n1 -s
+	echo -e "\e[0m"
+	sudo mkfs.ext4 /dev/sda1
 fdisk /dev/sda1 << EOF
 wipefs
 EOF
@@ -17,15 +21,11 @@ o
 n
 p
 1
-
+1
 
 w
 EOF
-	echo -e "\e[32m"
-	read -p "If it asks to overwrite anything just hit yes, make sure there are no needed files on the hd. Press any key to start the script..." -n1 -s
-	clear
-	echo -e "\e[0m"
-	sudo mkfs.ext4 /dev/sda1
+
 	dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda1
 	sed -i 's|/dev/mmcblk0p2|/dev/sda1|g' /etc/fstab
 else
