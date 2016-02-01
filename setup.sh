52d5640b40b63f10b706e401ef6f1c0dce0a11a6
Ezra <ezraholm50@gmail.com>
2016-02-01 11:00:58 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -644,67 +644,50 @@ apt-get update
 apt-get upgrade -y
 
 # Cleanup 1
-apt-get autoremove -y
-CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y purge)
-echo "$CLEARBOOT"
-clear
-
+#apt-get autoremove -y
+#CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | xargs sudo apt-get -y purge)
+#echo "$CLEARBOOT"
+#clear
+#
 # Use external harddrive to mount os and sd card to boot
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition? Also attach it before typing yes!!") ]]
-then
-# Use external harddrive to mount os and sd card to boot
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition? Also attach it before typing yes!!") ]]
-then
-# Use external harddrive to mount os and sd card to boot
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition? Also attach it before typing yes!!") ]]
-then
+#function ask_yes_or_no() {
+#    read -p "$1 ([y]es or [N]o): "
+#    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+#        y|yes) echo "yes" ;;
+#        *)     echo "no" ;;
+#    esac
+#}
+#if [[ "yes" == $(ask_yes_or_no "Do you want to use an external HD for ROOT partition (format to fat/ext4 first)? Also attach it before typing yes!!") ]]
+#then
 # Format and create partition
-fdisk /dev/sda1 << EOF
-wipefs
-EOF
-fdisk /dev/sda1 << EOF
-o
-n
-p
-1
 
+#fdisk /dev/sda1 << EOF
+#wipefs
+#EOF
 
-w
-EOF
-	echo -e "\e[32m"
-	read -p "If it asks to overwrite anything just hit yes, make sure there are no needed files on the hd. Press any key to start the script..." -n1 -s
-	echo -e "\e[0m"
-	sudo mkfs.ext4 /dev/sda1
-	dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda1
-	sed -i 's|/dev/mmcblk0p2|/dev/sda1|g' /etc/fstab
-else
-echo
-    echo "If you want to do this later: bash /var/scripts/usbhd.sh"
-    echo -e "\e[32m"
-    read -p "Press any key to continue... " -n1 -s
-    echo -e "\e[0m"
-fi
+#fdisk /dev/sda1 << EOF
+#o
+#n
+#p
+#
+#
+#
+#w
+#EOF
+#	echo -e "\e[32m"
+#	read -p "If it asks to overwrite anything just hit yes, make sure there are no needed files on the hd. Press any key to start the script..." -n1 -s
+#	echo -e "\e[0m"
+#	sudo mkfs.ext4 -n 'PI_ROOT' -I /dev/sda1
+#	sudo mount /dev
+#	dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda1
+#	sed -i 's|/dev/mmcblk0p2|/dev/sda1|g' /etc/fstab
+#else
+#echo
+#    echo "If you want to do this later: bash /var/scripts/usbhd.sh"
+#    echo -e "\e[32m"
+#    read -p "Press any key to continue... " -n1 -s
+#    echo -e "\e[0m"
+#fi
 
 #echo "We are now setting up your USB harddrive to mount the os, please attach your USB harddrive now, if you have not done so."
 #echo -e "\e[32m"
