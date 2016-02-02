2a7b01b9b306d7ce05c2608e8a79645c5281f49a
Ezra <ezraholm50@gmail.com>
2016-02-02 21:41:14 +0100

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -21,6 +21,7 @@ sed -i "s|# deb http://ports.ubuntu.com/ubuntu-ports/ vivid-security multiverse|
 sed -i "s|# deb-src http://ports.ubuntu.com/ubuntu-ports/ vivid-security multiverse|deb-src http://ports.ubuntu.com/ubuntu-ports/ vivid-security multiverse|g" /etc/apt/sources.list
 
 sudo apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get autoclean -y
+sudo apt-get install openssh-server -y
 #sudo useradd -d /home/$USERNAME -m $USERNAME && sudo usermod -aG sudo $USERNAME && echo $USERNAME:$USERPASS | chpasswd
 
 # Resize sd card
@@ -40,5 +41,28 @@ cd /tmp
 wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20150131-5_all.deb
 apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 -y
 dpkg -i raspi-config_20150131-5_all.deb
+# Overclock
+echo -e "\e[32m"
+echo    "+--------------------------------------------------------------------+"
+echo    "| I recommend you to use one of the overclock settings, which do not |"
+echo    "| Void warrenty as stated on the RPI2 site. If you want to use the   |"
+echo    "| Max overclock settings, visit BerryCloud @ gitgub                  |"
+echo    "+--------------------------------------------------------------------+"
+echo
+read -p "Press any key to enter overclock menu (only overclock dont use other settings and don't update, it will break overclocking)..." -n1 -s
+echo -e "\e[0m"
+echo
 raspi-config
+
+# Success!
+echo -e "\e[32m"
+echo    "+--------------------------------------------------------------------+"
+echo    "| Your ip is $ADDRESS ssh is now install so you can disconnect your  |"
+echo    "| Monitor and continue the installation over ssh, after reboot.      |"
+echo    "| After this reboot log back in with ocadmin///owncloud              |"
+echo    "+--------------------------------------------------------------------+"
+echo
+read -p "Press any key to reboot..." -n1 -s
+echo -e "\e[0m"
+echo
 sudo reboot
