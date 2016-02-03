62a6d81f57c3b0d14f1f1f0486f31c52b420a3db
Ezra <ezraholm50@gmail.com>
2016-02-03 20:38:32 +0100

    Create pre1.sh

---
+++ pre1.sh
@@ -0,0 +1,11 @@
+# Pre
+echo -e "\e[32m"
+echo    "+--------------------------------------------------------------------+"
+echo    "| A pre-setup script will run now followed by a reboot.              |"
+echo    "| After the reboot log back in with ocadmin///owncloud               |"
+echo    "| Then the setup script will run.                                    |"
+echo    "+--------------------------------------------------------------------+"
+echo
+read -p "Press any key to continue..." -n1 -s
+echo -e "\e[0m"
+echo
