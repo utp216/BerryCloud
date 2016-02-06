568f656b21b7f2d7111438b3105be36264eeae50
Ezra <ezraholm50@gmail.com>
2016-02-06 23:17:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -57,11 +57,11 @@ mkswap /swapfile
 swapon /swapfile
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
 
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoclean && apt-get clean -y && apt-get autoremove -y && apt-get -f install -y
+sudo apt-get autoremove -y && apt-get autoclean -y && apt-get update && apt-get upgrade -y && && apt-get -f install -y
 #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 #sudo apt-get update
 sudo apt-get install -y software-properties-common ifupdown openssh-server add-apt-repository python-software-properties clamav net-tools git linux-firmware dnsutils language-pack-en-base expect aptitude lvm2 ntp curl initscripts keyboard-configuration
-sudo apt-get update && sudo apt-get upgrade -y apt-get autoremove -y && apt-get -f install -y
+sudo apt-get autoremove -y && apt-get autoclean -y && apt-get update && apt-get upgrade -y && && apt-get -f install -y
 
 # Remove locale error over ssh in other language
 sed -i 's|    SendEnv LANG LC_*|#   SendEnv LANG LC_*|g' /etc/ssh/ssh_config
