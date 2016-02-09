e47190f054e9b250c8f05eda189f1e7b7ebe2b4e
Ezra <ezraholm50@gmail.com>
2016-02-09 13:47:45 +0100

    Create setup_secure_permissions_owncloud.sh

---
+++ setup_secure_permissions_owncloud.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+ocpath='/var/www/html/owncloud'
+htuser='www-data'
+htgroup='www-data'
+rootuser='root'
+data='/owncloud'
+
+printf "Creating possible missing Directories\n"
+mkdir -p $data
+mkdir -p $ocpath/assets
+mkdir -p $data/data
+
+printf "chmod Files and Directories\n"
+find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+find $data/ -type f -print0 | xargs -0 chmod 0640
+find $data/ -type d -print0 | xargs -0 chmod 0750
+
+printf "chown Directories\n"
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${rootuser}:${htgroup} $data/
+chown -R ${htuser}:${htgroup} $data/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+chown -R ${htuser}:${htgroup} ${ocpath}/assets/
+
+chmod +x ${ocpath}/occ
+
+printf "chmod/chown .htaccess\n"
+if [ -f ${ocpath}/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
+fi
+if [ -f ${ocpath}/data/.htaccess ]
+ then
+  chmod 0644 ${ocpath}/data/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+fi
