3f642fdf1bfe8303429832e8f85aafd2fc0d26b7
Ezra <ezraholm50@gmail.com>
2016-02-09 10:36:04 +0100

    Update pre1.sh

--- pre1.sh
+++ pre1.sh
@@ -1,7 +1,7 @@
 # Pre
 echo -e "\e[32m"
 echo    "+--------------------------------------------------------------------+"
-echo    "| After pressing enter, enter password: owncloud                     |"
+echo    "| After pressing any key, enter password: owncloud                   |"
 echo    "| A pre-setup script will run now followed by a reboot.              |"
 echo    "| After the reboot log back in with ocadmin///owncloud               |"
 echo    "| Then the setup script will run.                                    |"
