9b74d9cb7a0879eca100a5969d54a2569252ff22
Ezra <ezraholm50@gmail.com>
2016-02-10 22:47:59 +0100

    Update usbhd.sh

--- usbhd.sh
+++ usbhd.sh
@@ -38,15 +38,9 @@ echo "/dev/sda1 none swap sw 0 0" >> /etc/fstab
 
 # Update tables
 partprobe
-sed -i 's|bash /var/scripts/usbhd.sh|#bash /var/scripts/usbhd.sh|g' /root/.profile
+sync
+#sed -i 's|bash /var/scripts/usbhd.sh|#bash /var/scripts/usbhd.sh|g' /root/.profile
 
-# External HD	
-echo "This might take a while, copying everything from SD card to HD. Just wait untill system continues."
-sleep 2
-echo -ne '\n' | sudo mke2fs -t ext4 -b 4096 -L 'PI_ROOT' /dev/sda2 # make ext4 partition to hold ROOT
-dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda2 # copy the content of the SD ROOT partition to the new HD ROOT partition
-sed -i 's|/dev/mmcblk0p2|/dev/sda2|g' /etc/fstab # change ROOT device so the system will know which one to use as ROOT
-	
 # Change back root/.profile
 ROOT_PROFILE="/root/.profile"
 
@@ -66,17 +60,28 @@ if [ -x /var/scripts/history.sh ]; then
         /var/scripts/history.sh
 fi
 mesg n
+bash /var/scripts/pre.sh
 ROOT-PROFILE
 
-else
-echo
-# Install swapfile of 2 GB
-fallocate -l 2048M /swapfile # create swapfile and set size
-chmod 600 /swapfile # give it the right permissions
-mkswap /swapfile # format as swap
-swapon /swapfile # announce to system
-echo "/swapfile none swap defaults 0 0" >> /etc/fstab # let the system know what file to use as swap after reboot
-fi
+# External HD	
+echo "This might take a while, copying everything from SD card to HD. Just wait untill system continues."
+sleep 2
+echo -ne '\n' | sudo mke2fs -t ext4 -b 4096 -L 'PI_ROOT' /dev/sda2 # make ext4 partition to hold ROOT
+dd bs=4M conv=sync,noerror if=/dev/mmcblk0p2 of=/dev/sda2 # copy the content of the SD ROOT partition to the new HD ROOT partition
+sed -i 's|/dev/mmcblk0p2|/dev/sda2|g' /etc/fstab # change ROOT device so the system will know which one to use as ROOT
+
+# Set cmdline.txt
+mount /dev/mmcblk0p1 /mnt
+echo "root=/dev/sda2 rootfstype=ext4 bootdelay rootdelay rootwait" >> /mnt/cmdline.txt
+umount /mnt
+
+# Remove SD card ROOT partition
+fdisk /dev/mmcblk0 << EOF
+d
+2
+w
+EOF
+
  echo -e "\e[32m"
 echo    "+--------------------------------------------------------------------+"
 echo    "| After this reboot log back in with ocadmin///owncloud              |"
@@ -87,3 +92,13 @@ read -p "Press any key to continue..." -n1 -s
 echo -e "\e[0m"
 echo
 reboot
+
+else
+echo
+# Install swapfile of 2 GB
+fallocate -l 2048M /swapfile # create swapfile and set size
+chmod 600 /swapfile # give it the right permissions
+mkswap /swapfile # format as swap
+swapon /swapfile # announce to system
+echo "/swapfile none swap defaults 0 0" >> /etc/fstab # let the system know what file to use as swap after reboot
+fi
