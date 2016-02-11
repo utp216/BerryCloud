7159e8e4f346f370e7a89c9775ae7fdd736c405c
Ezra <ezraholm50@gmail.com>
2016-02-11 20:34:27 +0100

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -356,4 +356,29 @@ sudo chown ocadmin:ocadmin /home/ocadmin/.profile
 
 sleep 60
 
+# Change back rc.local
+rm /etc/rc.local
+cat << RCLOCAL > "/etc/rc.local"
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+
+RCLOCAL
+
+# Set permissions for rc.local
+chmod 755 /etc/rc.local
+
+reboot
+
 exit 0
