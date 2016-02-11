b63369043790d67c38c5f44857891e66f4248e41
Ezra <ezraholm50@gmail.com>
2016-02-11 22:55:08 +0100

    Update set_overclock.sh

--- set_overclock.sh
+++ set_overclock.sh
@@ -21,16 +21,18 @@ then
   }
   if [[ "yes" == $(ask_yes_or_no "Do you want to overclock and keep warranty on your device, enter yes. If not for the best speed enter no. (There's a safety option build in, when RPI reaches over 85 degrees celcius it uses default settings)") ]]
   then
+    sync
     mount /dev/mmcblk0p1 /mnt
-    rm config.txt
-    wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/config_warranty_overclock.txt -P /mnt
-    mv /mnt/config_warranty_overclock.txt /mnt/config.txt
+    rm /mnt/config.txt
+    wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/config_warranty_overclock.txt -P /tmp
+    mv /tmp/config_warranty_overclock.txt /mnt/config.txt
     umount /mnt
   else
+    sync
     mount /dev/mmcblk0p1 /mnt
-    rm config.txt
-    wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/config_warrenty_void.txt -P /mnt
-    mv /mnt/config_warranty_void.txt /mnt/config.txt
+    rm /mnt/config.txt
+    wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/config_warranty_void.txt -P /tmp
+    mv /tmp/config_warranty_void.txt /mnt/config.txt
     umount /mnt
   fi
 #######################################  
