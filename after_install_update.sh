24f12a81cc10db8f1e3a5fa39ba74f6a1256587d
Ezra <ezraholm50@gmail.com>
2016-02-11 09:21:50 +0100

    Create after_install_update.sh

---
+++ after_install_update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Tech and Me, 2016 - www.techandme.se
+#
+# This script contains edit/addons after the initial release per branch.
+# Use this script to update your system after installing any release so you
+# wont have to reinstall your entire system
+
+exit 0
