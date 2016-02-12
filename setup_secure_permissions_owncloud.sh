d2ebd9803884fc22c86c425aa89d0f118276e123
Ezra <ezraholm50@gmail.com>
2016-02-12 15:42:14 +0100

    Update setup_secure_permissions_owncloud.sh

--- setup_secure_permissions_owncloud.sh
+++ setup_secure_permissions_owncloud.sh
@@ -7,24 +7,29 @@ htuser='www-data'
 htgroup='www-data'
 rootuser='root'
 data='/owncloud'
+data.old='/var/www/html/owncloud/data'
 
 printf "Creating possible missing Directories\n"
 mkdir -p $data
 mkdir -p $ocpath/assets
 mkdir -p $data/data
+mkdir -p $data.old
 
 printf "chmod Files and Directories\n"
 find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
 find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
-find $data/ -type f -print0 | xargs -0 chmod 0640
-find $data/ -type d -print0 | xargs -0 chmod 0750
+find ${data}/ -type f -print0 | xargs -0 chmod 0640
+find ${data}/ -type d -print0 | xargs -0 chmod 0750
+find ${data.old}/ -type f -print0 | xargs -0 chmod 0640
+find ${data.old}/ -type d -print0 | xargs -0 chmod 0750
 
 printf "chown Directories\n"
 chown -R ${rootuser}:${htgroup} ${ocpath}/
 chown -R ${htuser}:${htgroup} ${ocpath}/apps/
 chown -R ${htuser}:${htgroup} ${ocpath}/config/
-chown -R ${htgroup}:${htgroup} $data/
+chown -R ${rootuser}:${htgroup} $data/
 chown -R ${htuser}:${htgroup} $data/data/
+chown -R ${htuser}:${htgroup} $data.old/
 chown -R ${htuser}:${htgroup} ${ocpath}/themes/
 chown -R ${htuser}:${htgroup} ${ocpath}/assets/
 
@@ -38,6 +43,11 @@ if [ -f ${ocpath}/.htaccess ]
 fi
 if [ -f ${ocpath}/data/.htaccess ]
  then
-  chmod 0644 ${ocpath}/data/.htaccess
-  chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
+  chmod 0644 ${data.old}/.htaccess
+  chown ${rootuser}:${htgroup} ${data.old}/.htaccess
+fi
+if [ -f ${data}/.htaccess ]
+ then
+  chmod 0644 ${data}/.htaccess
+  chown ${rootuser}:${htgroup} ${data}/.htaccess
 fi
