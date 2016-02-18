834f2f33dd9c4d3c0b39793f09ac975b15db573d
Ezra <ezraholm50@gmail.com>
2016-02-18 17:49:43 +0100

    Update update_checker.sh

--- update_checker.sh
+++ update_checker.sh
@@ -4,24 +4,25 @@
 # Tech and Me, 2016 - www.techandme.se
 #
 # Use external harddrive to mount os and sd card to boot
-function ask_yes_or_no() {
-    read -p "$1 ([y]es or [N]o): "
-    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-        y|yes) echo "yes" ;;
-        *)     echo "no" ;;
-    esac
-}
-if [[ "yes" == $(ask_yes_or_no "An update is available, download and install now?") ]]
-then
-echo
-echo "Please enter your password below"
-echo
-sudo -i
-wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/update.sh
-chmod 750 /var/scripts/update.sh
-bash /var/scripts/update.sh
-else
-sleep 1
-fi
-
-exit 0
+#function ask_yes_or_no() {
+#    read -p "$1 ([y]es or [N]o): "
+#    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+#        y|yes) echo "yes" ;;
+#        *)     echo "no" ;;
+#    esac
+#}
+#if [[ "yes" == $(ask_yes_or_no "An update is available, download and install now?") ]]
+#then
+#echo
+#echo "Please enter your password below"
+#echo
+#sudo -i
+#wget https://raw.githubusercontent.com/ezraholm50/BerryCloud/master/update.sh
+#chmod 750 /var/scripts/update.sh
+#bash /var/scripts/update.sh
+#else
+#sleep 1
+#fi
+#
+#exit 0
+#
